f1c8839dd0a8ca0df0eb0af51474d4daff5093ba
fredylsmann <ferdiarmond@gmail.com>
2026-05-08 02:45:13 +0700

    Build: Standardize path resolution in vite.config.js to fix Vercel parse error

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
   ],
   resolve: {
     alias: {
-      '@': '/src'
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1600,
   },
   server: {
     port: 5173,
